3ce3ab8c6cddbe6101d9a41897c1eb30c8f3eb9f
rbelmeha <belmega31@gmail.com>
2019-07-15 22:48:21 +0300

    fix typo

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,6 @@
 import config from "react-native-config";
-import endpoints from "../constants/endpoits";
+import endpoints from "../constants/endpoints";
+import { request } from "./request";
 
 export const getUserByCarNumber = number => {
   const url = `${config.NODE_SERVER}/${endpoints.carNumber}`;
