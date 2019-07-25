d7462b8f11774afe64c538252094372f39e44f5e
rostyslavbelmeha <belmega31@gmail.com>
2019-07-25 21:38:46 +0300

    using accessToken

--- src/services/request.js
+++ src/services/request.js
@@ -2,7 +2,7 @@ import { AsyncStorage } from "react-native";
 
 export const request = async ({ url, method, body }) => {
   const userToken = await AsyncStorage.getItem("userToken");
-  const token = JSON.parse(userToken) && JSON.parse(userToken).idToken;
+  const token = JSON.parse(userToken) && JSON.parse(userToken).accessToken;
 
   return fetch(url, {
     method,
