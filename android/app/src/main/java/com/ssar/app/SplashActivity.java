f17d5bf04128d499d385d7685318bf9a9cc05251
Yuliia Nesteruk <julia.nesteruk@gmail.com>
2019-09-09 16:53:24 +0300

    android splash to look similar to ios

--- android/app/src/main/java/com/ssar/app/SplashActivity.java
+++ android/app/src/main/java/com/ssar/app/SplashActivity.java
@@ -1,16 +1,26 @@
 package com.ssar.app;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Handler;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
+
+    private static int SPLASH_TIME_OUT = 3000;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState){
-         super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
 
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
