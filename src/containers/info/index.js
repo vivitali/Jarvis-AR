e3f8409b4797e7968eba1b0402c7a0156a48fc90
anna-kuzii <anna_kuzij@ukr.net>
2019-10-23 16:07:37 +0300

    update help screen

--- src/containers/info/index.js
+++ src/containers/info/index.js
@@ -5,7 +5,7 @@ import { ImageBackground, Text, TouchableOpacity, View } from "react-native";
 import Button from "apsl-react-native-button";
 // $FlowFixMe
 import Icon from "react-native-vector-icons/FontAwesome";
-import styles from "./style";
+import styles from "./styles";
 
 import Layout from "../../constants/Layout";
 
